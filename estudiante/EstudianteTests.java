f7d4102b7639a98b10ba8c77a9c0b28b545879f8
Meliton Pablo <melitonio8@hotmail.com>
2024-05-11 18:39:44 +0100

    Mejoras en los test

--- estudiante/EstudianteTests.java
+++ estudiante/EstudianteTests.java
@@ -4,36 +4,36 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class EstudianteTests {
-    
-        @Test
-        public void testNombre() {
-            Estudiante estudiante = new Estudiante("Juan", "123");
-            assertEquals(estudiante.getNombre(), "Juan");
-        }
-    
-        @Test
-        public void testId() {
-            Estudiante estudiante = new Estudiante("Juan", "123");
-            assertEquals(estudiante.getId(), "123");
-        }
-    
-        @Test
-        public void testCreditos() {
-            Estudiante estudiante = new Estudiante("Juan", "123");
-            assertEquals(estudiante.getCreditos(), 0);
-        }
-    
-        @Test
-        public void testNombreUsuario() {
-            Estudiante estudiante = new Estudiante("Juan", "123");
-            System.out.println(estudiante.getNombreUsuario());
-            assertEquals(estudiante.getNombreUsuario(), "Juan123");
-        }
-    
-        @Test
-        public void testSumarCreditos() {
-            Estudiante estudiante = new Estudiante("Juan", "123");
-            estudiante.sumarCreditos(10);
-            assertEquals(estudiante.getCreditos(), 10);
-        }
+
+    @Test
+    public void testNombre() {
+        Estudiante estudiante = new Estudiante("Juan", "123");
+        assertEquals(estudiante.getNombre(), "Juan");
+    }
+
+    @Test
+    public void testId() {
+        Estudiante estudiante = new Estudiante("Juan", "123");
+        assertEquals(estudiante.getId(), "123");
+    }
+
+    @Test
+    public void testCreditos() {
+        Estudiante estudiante = new Estudiante("Juan", "123");
+        assertEquals(estudiante.getCreditos(), 0);
+    }
+
+    @Test
+    public void testNombreUsuario() {
+        Estudiante estudiante = new Estudiante("Juan", "123");
+        System.out.println(estudiante.getNombreUsuario());
+        assertEquals(estudiante.getNombreUsuario(), "Juan123");
+    }
+
+    @Test
+    public void testSumarCreditos() {
+        Estudiante estudiante = new Estudiante("Juan", "123");
+        estudiante.sumarCreditos(10);
+        assertEquals(estudiante.getCreditos(), 10);
+    }
 }
